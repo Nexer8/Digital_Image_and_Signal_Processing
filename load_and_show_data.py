37dfc278bae370c53064e475fca534dd27b17247
Nexer8 <PrideFlatFace99>
2020-03-08 22:49:56 +0100

    Program that reads the data, shows them on a plot and allows the user to show only specified area
    Signed-off-by: Nexer8 <PrideFlatFace99>

--- load_and_show_data.py
+++ load_and_show_data.py
@@ -7,27 +7,28 @@ import numpy as np
 
 def load_and_show_data():
     ecg_data = [[] for i in range(12)]
-    frequency = 800    # set the frequency to 800 Hz
+    frequency = 800  # set the frequency to 800 Hz
 
-    data = np.loadtxt('Data/ekg1.txt', delimiter=" ")
+    data = np.loadtxt("Data/ekg1.txt", delimiter=" ")
     for row in data:
         for i, ecg in enumerate(ecg_data):
             ecg.append(row[i])
 
-    time = np.arange(len(ecg_data[0])) / frequency  # Create an array of equal periods of time
+    time = (
+        np.arange(len(ecg_data[0])) / frequency
+    )  # Create an array of equal periods of time
 
     for i, ecg in enumerate(ecg_data):
         plt.plot(time, ecg)
         plt.xlabel("time [s]")
         plt.ylabel("ECG [mV]")
         plt.xlim(0, 0.1)  # select just a part of x axis to render
-        plt.ylim(0, 200)   # select just a part of y axis to render
+        plt.ylim(0, 200)  # select just a part of y axis to render
         plt.savefig(f"Results/graph{i}.png")
         # plt.show()
 
 
 def main():
-    print("Hello")
     load_and_show_data()
 
 
